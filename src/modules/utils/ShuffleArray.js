fbdc0f91c1bb5a81f0c98abc83919b0f774c7dc5
Eetu Aaltonen <eetu.aaltonen98@gmail.com>
2024-12-18 13:22:37 +0200

    Add utility function to shuffle given array. Fix JSON struct array parse function to check failed parsings

---
+++ src/modules/utils/ShuffleArray.js
@@ -0,0 +1,12 @@
+/**
+ * Shuffles a given array randomly
+ * @param {Array<any>} array
+ * @return {Array<any>} Shuffled array
+ */
+export default function (array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
